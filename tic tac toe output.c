97c31fa3a6ff5f056e90c1ca8917ce87ea4ac1d0
VelishalaVyshnavi04 <82885884+VelishalaVyshnavi04@users.noreply.github.com>
2021-04-20 19:14:03 +0530

    Add files via upload

---
+++ tic tac toe output.c
@@ -0,0 +1,16 @@
+Tic Tac Toe
+
+Player 1 (X)  -  Player 2 (Y)
+
+
+     |     |
+  X  |  O  |  X
+__|_|__
+     |     |
+  O  |  O  |  X
+__|_|__
+     |     |
+  O  |  X  |  X
+     |     |
+
+==>Player 1 win
